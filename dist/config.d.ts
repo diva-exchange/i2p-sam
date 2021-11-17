ee1feddc44186452c7e772a372f4b92f54c06200
Konrad Baechler <konrad@diva.exchange>
2021-11-17 18:56:20 +0100

    changed: linted, node 12 compat

--- dist/config.d.ts
+++ dist/config.d.ts
@@ -1,45 +1,45 @@
 declare type tSession = {
-    id?: string;
+  id?: string;
 };
 declare type tStream = {
-    destination: string;
-    onMessage?: Function;
-    onError?: Function;
-    onClose?: Function;
+  destination: string;
+  onMessage?: Function;
+  onError?: Function;
+  onClose?: Function;
 };
 declare type tListen = {
-    address: string;
-    port: number;
-    hostForward?: string;
-    portForward?: number;
-    onMessage?: Function;
-    onError?: Function;
-    onClose?: Function;
+  address: string;
+  port: number;
+  hostForward?: string;
+  portForward?: number;
+  onMessage?: Function;
+  onError?: Function;
+  onClose?: Function;
 };
 declare type tSam = {
-    host: string;
-    portTCP: number;
-    portUDP?: number;
-    versionMin?: string;
-    versionMax?: string;
-    publicKey?: string;
-    privateKey?: string;
-    onError?: Function;
-    onClose?: Function;
+  host: string;
+  portTCP: number;
+  portUDP?: number;
+  versionMin?: string;
+  versionMax?: string;
+  publicKey?: string;
+  privateKey?: string;
+  onError?: Function;
+  onClose?: Function;
 };
 export declare type Configuration = {
-    session?: tSession;
-    stream?: tStream;
-    listen?: tListen;
-    sam?: tSam;
+  session?: tSession;
+  stream?: tStream;
+  listen?: tListen;
+  sam?: tSam;
 };
 export declare class Config {
-    session: tSession;
-    stream: tStream;
-    listen: tListen;
-    sam: tSam;
-    constructor(c: Configuration);
-    private static b;
-    private static port;
+  session: tSession;
+  stream: tStream;
+  listen: tListen;
+  sam: tSam;
+  constructor(c: Configuration);
+  private static b;
+  private static port;
 }
 export {};
--- dist/i2p-sam-stream.d.ts
+++ dist/i2p-sam-stream.d.ts
@@ -2,11 +2,11 @@
 import { I2pSam } from './i2p-sam';
 import { Configuration } from './config';
 export declare class I2pSamStream extends I2pSam {
-    private socketStream;
-    private destination;
-    private hasStream;
-    static make(c: Configuration): Promise<I2pSamStream>;
-    protected open(): Promise<I2pSamStream>;
-    private connect;
-    send(msg: Buffer): void;
+  private socketStream;
+  private destination;
+  private hasStream;
+  static make(c: Configuration): Promise<I2pSamStream>;
+  protected open(): Promise<I2pSamStream>;
+  private connect;
+  send(msg: Buffer): void;
 }
